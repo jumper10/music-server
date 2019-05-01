ab2105f49b458f1364873afe9dc633a254bd9f72
jumper10 <jumper10@163.com>
2019-05-01 21:06:31 +0800

    enter

---
+++ src/main/java/com/music/server/ServerApplication.java
@@ -0,0 +1,23 @@
+package com.music.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * = ServerApplication
+ *
+ * TODO Auto-generated class documentation
+ *
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    /**
+     * TODO Auto-generated method documentation
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+}
