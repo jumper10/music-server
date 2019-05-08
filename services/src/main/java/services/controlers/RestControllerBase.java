4eb95bdc7dbde13d51c99113709303b9166afa33
jumper10 <jumper10@163.com>
2019-05-08 17:47:14 +0800

    1,add swagger

--- services/src/main/java/services/controlers/RestControllerBase.java
+++ services/src/main/java/services/controlers/RestControllerBase.java
@@ -3,6 +3,6 @@ package services.controlers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/api")
-public class RestControlerBase {
+
+abstract class RestControllerBase {
 }
--- services/src/main/java/services/controlers/SmokeController.java
+++ services/src/main/java/services/controlers/SmokeController.java
@@ -1,20 +1,20 @@
 package services.controlers;
 
+
 import data.access.mapper.SmokeMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
 @RequestMapping("/smoke")
-public class SmokeControler extends RestControlerBase {
+@RestController
+public class SmokeController extends RestControllerBase {
     @Autowired
     SmokeMapper smokeMapper;
 
-
-
-    @GetMapping("/")
+    @GetMapping
     public String Smoke(){
         return  smokeMapper.smoke();
     }
